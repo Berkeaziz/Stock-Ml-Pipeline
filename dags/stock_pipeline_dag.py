21cc61cc6fe0908c0f718b214a2db645b9790902
berkeaziz <berke-aziz2001@hotmail.com>
2026-02-10 00:54:40 +0300

    AIRFLOW Testing

--- dags/stock_pipeline_dag.py
+++ dags/stock_pipeline_dag.py
@@ -1,41 +1,46 @@
 from datetime import datetime
-from airflow import dag
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+# Project imports
 from src.etl.run_etl import main as etl_main
 from src.qualities.checks import run_quality_checks
 from src.features.run_features import main as features_main
 from src.train.train import main as train_main
 from src.config import DB_URL
 
-default_args ={"owner":"berke","retries":1}
+default_args = {
+    "owner": "berke",
+    "retries": 1,
+}
 
 with DAG(
     dag_id="stock_ml_pipeline",
     default_args=default_args,
     start_date=datetime(2025, 1, 1),
-    schedule=None,  # manuel trigger (istersen sonra weekly yaparız)
+    schedule=None,  # manual trigger
     catchup=False,
     tags=["de", "ds", "ml"],
 ) as dag:
-    
-    etl=PythonOperator(
-        task_id ="etl_extract_transform_load",
+
+    etl = PythonOperator(
+        task_id="etl_extract_transform_load",
         python_callable=etl_main,
     )
 
-    quality =PythonOperator(
-        task_id ="data_quality_checks",
-        python_callable=lambda:run_quality_checks(DB_URL)
+    quality = PythonOperator(
+        task_id="data_quality_checks",
+        python_callable=lambda: run_quality_checks(DB_URL),
     )
 
-    features= PythonOperator(
-        task_id = "feature_engineering",
-        python_callable =features_main,
+    features = PythonOperator(
+        task_id="feature_engineering",
+        python_callable=features_main,
     )
-        train = PythonOperator(
+
+    train = PythonOperator(
         task_id="train_model",
         python_callable=train_main,
     )
 
-    etl>>quality>>features>>train
+    etl >> quality >> features >> train
--- src/config.py
+++ src/config.py
@@ -1,16 +1,19 @@
-import os 
+import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Airflow container içinde proje buraya mount ediliyor:
+PROJECT_ROOT = os.getenv("PROJECT_ROOT", "/opt/airflow/project")
+ENV_PATH = os.path.join(PROJECT_ROOT, ".env")
 
-DB_USER = os.getenv("POSTGRES_USER")
-DB_PASSWORD =os.getenv("POSTGRES_PASSWORD")
-DB_NAME = os.getenv("POSTGRES_DB")
-DB_PORT = os.getenv("POSTGRES_PORT")
-DB_HOST = os.getenv("DB_HOST", "localhost")
+load_dotenv(dotenv_path=ENV_PATH)  
 
-ALPHAVANTAGE_API_KEY=os.getenv("ALPHAVANTAGE_API_KEY")
-SYMBOL = os.getenv("SYMBOL")
+DB_USER = os.getenv("POSTGRES_USER", "berke166")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "depass")
+DB_NAME = os.getenv("POSTGRES_DB", "berke_db")
+DB_PORT = os.getenv("POSTGRES_PORT", "5435")
+DB_HOST = os.getenv("DB_HOST", "host.docker.internal")
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+SYMBOL = os.getenv("SYMBOL", "IBM")
 
 DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
